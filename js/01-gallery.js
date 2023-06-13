556aeae338e1428b32c55739815438b3fa1167e8
Anna <anna@Annas-MacBook-Air.local>
2023-06-13 12:53:03 -0700

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listItems = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const listLi = galleryItems.map(el => 
+
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${el.original}">
+            <img
+            class="gallery__image"
+            src="${el.preview}"
+            data-source="${el.original}"
+            alt="${el.description}"
+            />
+        </a>
+    </li>`);
+
+listItems.innerHTML = listLi.join("");
+
+listItems.addEventListener("click", (event) => {
+    event.preventDefault();
+    
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+    instance.show();
+}
+)
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listItems = document.querySelector(".gallery");
+
+const listLi = galleryItems.map(el => 
+
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${el.original}">
+            <img
+            class="gallery__image"
+            src="${el.preview}"
+            alt="${el.description}"
+            />
+        </a>
+    </li>`);
+
+listItems.innerHTML = listLi.join("");
+
 
 console.log(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+
+console.log(lightbox);
